ec9775c97cedc3cd4c2b8940094d84e19c3a162b
larsga <larsga@d0b0ecf4-2c1d-11de-ba90-e1c2dc4d389a>
2009-07-09 15:05:14 +0000

    Implemented initial skeleton. Now "only" missing support for scaling images.

--- trunk/src/java/no/priv/garshol/topicmaps/tmphoto/ImageServlet.java
+++ trunk/src/java/no/priv/garshol/topicmaps/tmphoto/ImageServlet.java
@@ -1,8 +1,23 @@
 
 package no.priv.garshol.topicmaps.tmphoto;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
 import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
+
+import net.ontopia.utils.StreamUtils;
+import net.ontopia.infoset.core.LocatorIF;
+import net.ontopia.topicmaps.core.*;
+import net.ontopia.topicmaps.entry.TopicMapReferenceIF;
+import net.ontopia.topicmaps.entry.TopicMapRepositoryIF;
+import net.ontopia.topicmaps.nav2.utils.NavigatorUtils;
+
+// full    : full
+// default : 800x800
+// thumb   : 250x250
 
 /**
  * Receives a request of the form tmphoto/image?id;size and returns
@@ -10,17 +25,66 @@ import javax.servlet.http.*;
  */
 public class ImageServlet extends HttpServlet {
 
+  final static private String CACHEDIR = null; // FIXME
+  
   protected void doGet(HttpServletRequest req,
                        HttpServletResponse resp)
     throws ServletException, IOException {
 
-    // get parameters
+    // initialize
     String id = getImageId(req);
     String size = getImageSize(req);
-    
-  }
+    String origfile = getImageFileName(id);
+    if (origfile == null) {
+      resp.sendError(404, "No such image");
+      return;
+    }
 
+    // get reference to scaled image (and scale, if necessary)
+    File scaledfile = getScaledFile(id, origfile, size);
+
+    // pump it out!
+    resp.setHeader("Content-type", "image/jpeg");
+    resp.setIntHeader("Content-length", (int) scaledfile.length());
+    OutputStream out = resp.getOutputStream();
+    FileInputStream in = new FileInputStream(scaledfile);
+    StreamUtils.transfer(in, out);
+    in.close();
+    out.flush();
+  }
+  
   // --- Internal helpers
+
+  private static File getScaledFile(String id, String origfile, String size) {
+    if (size.equals("full"))
+      return new File(origfile);
+
+    File scaledfile = new File(CACHEDIR + File.separator + id);
+    if (!scaledfile.exists()) {
+      // FIXME: scale file and store cached copy in scaledfile
+    }
+    return scaledfile;
+  }
+  
+  private String getImageFileName(String id) throws IOException {
+    // get topic map
+    TopicMapRepositoryIF repo =
+      NavigatorUtils.getTopicMapRepository(getServletContext());
+    TopicMapReferenceIF ref = repo.getReferenceByKey("metadata.xtm");
+    TopicMapStoreIF store = ref.createStore(true);
+    TopicMapIF tm = store.getTopicMap();
+
+    // get image topic
+    LocatorIF base = store.getBaseAddress();
+    LocatorIF iid = base.resolveAbsolute("#" + id);
+    TopicIF topic = (TopicIF) tm.getObjectByItemIdentifier(iid);
+    if (topic == null)
+      return null;
+
+    // locate image
+    LocatorIF subjloc = (LocatorIF) topic.getSubjectLocators().iterator().next();
+    return subjloc.getAddress().substring(6);
+  }
 
   private static String getImageId(HttpServletRequest req) {
     String query = req.getQueryString();
