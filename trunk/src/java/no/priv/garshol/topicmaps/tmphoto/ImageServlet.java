dd44dd86012afac448974903276151ae3afe6c6f
larsga <larsga@d0b0ecf4-2c1d-11de-ba90-e1c2dc4d389a>
2009-07-16 17:11:02 +0000

    Scaling images now works.

--- trunk/src/java/no/priv/garshol/topicmaps/tmphoto/ImageServlet.java
+++ trunk/src/java/no/priv/garshol/topicmaps/tmphoto/ImageServlet.java
@@ -3,10 +3,17 @@ package no.priv.garshol.topicmaps.tmphoto;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
+
+import java.awt.image.RenderedImage;
+import javax.media.jai.JAI;
+import javax.media.jai.Interpolation;
 
 import net.ontopia.utils.StreamUtils;
 import net.ontopia.infoset.core.LocatorIF;
@@ -15,17 +22,13 @@ import net.ontopia.topicmaps.entry.TopicMapReferenceIF;
 import net.ontopia.topicmaps.entry.TopicMapRepositoryIF;
 import net.ontopia.topicmaps.nav2.utils.NavigatorUtils;
 
-// full    : full
-// default : 800x800
-// thumb   : 250x250
-
 /**
  * Receives a request of the form tmphoto/image?id;size and returns
  * the corresponding scaled image. 
  */
 public class ImageServlet extends HttpServlet {
 
-  final static private String CACHEDIR = null; // FIXME
+  final static private String CACHEDIR = getCacheDir(); 
   
   protected void doGet(HttpServletRequest req,
                        HttpServletResponse resp)
@@ -59,11 +62,33 @@ public class ImageServlet extends HttpServlet {
     if (size.equals("full"))
       return new File(origfile);
 
-    File scaledfile = new File(CACHEDIR + File.separator + id);
-    if (!scaledfile.exists()) {
-      // FIXME: scale file and store cached copy in scaledfile
-    }
+    File scaledfile = new File(CACHEDIR + size + File.separator + id);
+    if (!scaledfile.exists())
+      scaleImage(new File(origfile), scaledfile, getMaxSide(size));
+    
     return scaledfile;
+  }
+
+  private static void scaleImage(File source, File destination, int maxside) {
+    RenderedImage src = JAI.create("fileload", source.getPath());
+    RenderedImage scaled = src;
+
+    int biggest = Math.max(src.getHeight(), src.getWidth());
+    float scale = maxside / (float) biggest;
+    if (scale < 1.0)
+      scaled = JAI.create("scale", src, scale, scale, 0, 0,
+                        Interpolation.getInstance(Interpolation.INTERP_BILINEAR));
+
+    JAI.create("filestore", scaled, destination.getPath(), "JPEG", null);
+  }
+
+  private static int getMaxSide(String size) {
+    if (size.equals("default"))
+      return 800;
+    else if (size.equals("thumb"))
+      return 250;
+    else
+      throw new RuntimeException("Unknown size: '" + size + "'");
   }
   
   private String getImageFileName(String id) throws IOException {
@@ -103,5 +128,20 @@ public class ImageServlet extends HttpServlet {
     else
       return query.substring(pos + 1);
   }
-  
+
+  private static String getCacheDir() {
+    String tmp = System.getProperty("java.io.tmpdir");
+    if (!tmp.endsWith(File.separator))
+      tmp += File.separator;
+
+    // create subdirs
+    File cache = new File(tmp + File.separator + "default");
+    if (!cache.exists())
+      cache.mkdir();
+    cache = new File(tmp + File.separator + "thumb");
+    if (!cache.exists())
+      cache.mkdir();
+    
+    return tmp;
+  }
 }
