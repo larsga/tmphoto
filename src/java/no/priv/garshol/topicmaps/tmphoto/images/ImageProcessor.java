ba39c8cd9023ec1962aee5de2a28e7a0a1dede69
larsga <larsga@d0b0ecf4-2c1d-11de-ba90-e1c2dc4d389a>
2009-07-19 13:43:31 +0000

    Added javadoc comment.

--- src/java/no/priv/garshol/topicmaps/tmphoto/images/ImageProcessor.java
+++ src/java/no/priv/garshol/topicmaps/tmphoto/images/ImageProcessor.java
@@ -11,9 +11,16 @@ import java.io.IOException;
  */
 public interface ImageProcessor {
 
+  /**
+   * Scales the image in the source file and stores the scaled image
+   * in the destination file. Aspect ratio is preserved, which is why
+   * size is only specified as the length of the longest side of the
+   * scaled image, using the maxside parameter.
+   */
   public void scaleImage(File source, File destination, int maxside)
     throws IOException;
 
+// used by tagger.py, so might be useful to add, for more flexibility
 //   public void rotateImage(File image, int direction)
 //     throws IOException;
   
