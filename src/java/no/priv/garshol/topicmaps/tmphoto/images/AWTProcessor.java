aaf672e75b02dbc9d074106c40de1560558fe12b
larsga <larsga@d0b0ecf4-2c1d-11de-ba90-e1c2dc4d389a>
2009-08-01 20:03:43 +0000

    Added AWT processor.

---
+++ src/java/no/priv/garshol/topicmaps/tmphoto/images/AWTProcessor.java
@@ -0,0 +1,33 @@
+
+package no.priv.garshol.topicmaps.tmphoto.images;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.geom.AffineTransform;
+
+import javax.imageio.ImageIO;
+
+/**
+ * Processor which calls uses the java.awt APIs to scale an image.
+ */
+public class AWTProcessor implements ImageProcessor {
+
+  public void scaleImage(File source, File destination, int maxside)
+    throws IOException {
+    BufferedImage bsrc = ImageIO.read(source);
+
+    double ratio = (double) maxside / Math.max(bsrc.getHeight(), bsrc.getWidth());
+    int width = (int) (bsrc.getWidth() * ratio);
+    int height = (int) (bsrc.getHeight() * ratio);
+    
+    BufferedImage bdest = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    Graphics2D g = bdest.createGraphics();
+    AffineTransform at =
+      AffineTransform.getScaleInstance(ratio, ratio);
+    g.drawRenderedImage(bsrc, at);
+    ImageIO.write(bdest, "JPG", destination);
+  }
+}
