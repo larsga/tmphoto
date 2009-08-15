7d36f928bad22bf221d9146273dec2844a923baa
larsga <larsga@d0b0ecf4-2c1d-11de-ba90-e1c2dc4d389a>
2009-08-15 11:19:21 +0000

    Extended RemoveBadVotes to also delete bad comments. Made RemoveBadVotes work again.

--- trunk/src/java/no/priv/garshol/topicmaps/tmphoto/RemoveBadVotes.java
+++ trunk/src/java/no/priv/garshol/topicmaps/tmphoto/RemoveBadVotes.java
@@ -20,24 +20,36 @@ import net.ontopia.topicmaps.core.TopicMapBuilderIF;
 import net.ontopia.topicmaps.utils.ImportExportUtils;
 
 /**
- * Command-line tool to delete votes for photos which no longer exist.
+ * Command-line tool to delete votes for photos which no longer exist,
+ * and also comments on deleted photos.
  */
 public class RemoveBadVotes {
   private static TopicMapIF topicmap;
+  private static LocatorIF base;
 
   public static void main(String[] args) throws IOException, SQLException {
+
+    JDBCUtils.init("jdbc:postgresql:tmphoto", "larsga", "secret");
+    
     topicmap = ImportExportUtils.getReader(args[0]).read();
+    base = topicmap.getStore().getBaseAddress();
     List votes = ScoreManager.getAllVotes();
     for (int ix = 0; ix < votes.size(); ix++) {
       ScoreManager.PhotoInList photo = (ScoreManager.PhotoInList) votes.get(ix);
       if (!topicExists(photo.getPhotoId()))
         ScoreManager.deleteVotesOn(photo.getPhotoId());
     }
+
+    List comments = CommentManager.getAllComments();
+    for (int ix = 0; ix < comments.size(); ix++) {
+      CommentManager.Comment comment = (CommentManager.Comment) comments.get(ix);
+      if (!topicExists(comment.getPhotoId()))
+        CommentManager.deleteComment(comment.getId());
+    }
   }
 
   private static boolean topicExists(String id) {
-    LocatorIF base = topicmap.getStore().getBaseAddress();
-    LocatorIF srcloc = base.resolveAbsolute("#" + id);
-    return topicmap.getObjectByItemIdentifier(srcloc) != null;
+    LocatorIF itemid = base.resolveAbsolute("#" + id);
+    return topicmap.getObjectByItemIdentifier(itemid) != null;
   }
 }
