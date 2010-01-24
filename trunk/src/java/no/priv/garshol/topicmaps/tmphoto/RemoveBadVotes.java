7ad4ef1032a15445fb8ccb7020cebac132becf68
larsga <larsga@d0b0ecf4-2c1d-11de-ba90-e1c2dc4d389a>
2010-01-24 17:46:57 +0000

    Rewritten so it can also be run as a servlet, to save memory so we don't meet the OOMKiller.

--- trunk/src/java/no/priv/garshol/topicmaps/tmphoto/RemoveBadVotes.java
+++ trunk/src/java/no/priv/garshol/topicmaps/tmphoto/RemoveBadVotes.java
@@ -11,28 +11,76 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
 import net.ontopia.utils.OntopiaRuntimeException;
 import net.ontopia.infoset.core.LocatorIF;
 import net.ontopia.infoset.impl.basic.URILocator;
 import net.ontopia.topicmaps.core.TopicIF;
 import net.ontopia.topicmaps.core.TopicMapIF;
+import net.ontopia.topicmaps.core.TopicMapStoreIF;
 import net.ontopia.topicmaps.core.TopicMapBuilderIF;
 import net.ontopia.topicmaps.utils.ImportExportUtils;
+import net.ontopia.topicmaps.entry.TopicMapReferenceIF;
+import net.ontopia.topicmaps.entry.TopicMapRepositoryIF;
+import net.ontopia.topicmaps.nav2.utils.NavigatorUtils;
 
 /**
- * Command-line tool to delete votes for photos which no longer exist,
- * and also comments on deleted photos.
+ * Tool to delete votes for photos which no longer exist, and also
+ * comments on deleted photos. Can be run from command-line or as
+ * a servlet.
  */
-public class RemoveBadVotes {
+public class RemoveBadVotes extends HttpServlet {
   private static TopicMapIF topicmap;
   private static LocatorIF base;
 
+  // command-line interface
   public static void main(String[] args) throws IOException, SQLException {
-
-    JDBCUtils.init("jdbc:postgresql:tmphoto", "larsga", "secret");
+    String jdbcurl = args[0];
+    String username = args[1];
+    String passwd = args[2];
+    
+    JDBCUtils.init(jdbcurl, username, passwd);
     
     topicmap = ImportExportUtils.getReader(args[0]).read();
     base = topicmap.getStore().getBaseAddress();
+    remove();
+  }
+
+  // servlet interface
+  protected void doGet(HttpServletRequest req,
+                       HttpServletResponse resp)
+    throws ServletException, IOException {
+    
+    ServletContext ctxt = getServletContext();
+    JDBCUtils.init(ctxt.getInitParameter("jdbcurl"),
+                   ctxt.getInitParameter("jdbcuser"),
+                   ctxt.getInitParameter("jdbcpasswd"));
+
+    TopicMapRepositoryIF repo =
+      NavigatorUtils.getTopicMapRepository(getServletContext());
+    TopicMapReferenceIF ref = repo.getReferenceByKey("metadata.xtm");
+    TopicMapStoreIF store = ref.createStore(true);
+    topicmap = store.getTopicMap();
+    base = store.getBaseAddress();
+
+    // set topicmap & base
+    try {
+      remove();
+    } catch (SQLException e) {
+      throw new ServletException(e);
+    }
+
+    // say something
+    String message = "Successful!";
+    resp.setHeader("Content-type", "text/plain");
+    resp.setIntHeader("Content-length", message.length());
+    resp.getWriter().write(message);
+  }
+
+  private static void remove() throws SQLException {
     List votes = ScoreManager.getAllVotes();
     for (int ix = 0; ix < votes.size(); ix++) {
       ScoreManager.PhotoInList photo = (ScoreManager.PhotoInList) votes.get(ix);
