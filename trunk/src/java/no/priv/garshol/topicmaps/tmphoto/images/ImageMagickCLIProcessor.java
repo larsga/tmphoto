1c2eb0ca2c840df7eaf7521a975e7eedea4a03d7
larsga <larsga@d0b0ecf4-2c1d-11de-ba90-e1c2dc4d389a>
2010-01-25 12:38:15 +0000

    Added proper error reporting.

--- trunk/src/java/no/priv/garshol/topicmaps/tmphoto/images/ImageMagickCLIProcessor.java
+++ trunk/src/java/no/priv/garshol/topicmaps/tmphoto/images/ImageMagickCLIProcessor.java
@@ -3,6 +3,8 @@ package no.priv.garshol.topicmaps.tmphoto.images;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import net.ontopia.utils.StreamUtils;
 
 /**
  * Processor which calls ImageMagick on the command-line.
@@ -21,7 +23,13 @@ public class ImageMagickCLIProcessor implements ImageProcessor {
                                  destination.getPath() };
     Process convert = Runtime.getRuntime().exec(cmd, null, null);
     try {
-      convert.waitFor();
+      int result = convert.waitFor();
+      if (result != 0) {
+        InputStreamReader reader =
+          new InputStreamReader(convert.getErrorStream());
+        String msg = StreamUtils.read(reader);
+        throw new IOException("Error in conversion: " + msg);
+      }
     } catch (InterruptedException e) {
     }
   }
