5ac99df1e415386a72607f094a39d005998adb23
larsga <larsga@d0b0ecf4-2c1d-11de-ba90-e1c2dc4d389a>
2010-06-07 20:03:29 +0000

    Added GraphicsMagick integration.

---
+++ src/java/no/priv/garshol/topicmaps/tmphoto/images/GraphicsMagickCLIProcessor.java
@@ -0,0 +1,38 @@
+
+package no.priv.garshol.topicmaps.tmphoto.images;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import net.ontopia.utils.StreamUtils;
+
+/**
+ * Processor which calls GraphicsMagick on the command-line.
+ */
+public class GraphicsMagickCLIProcessor implements ImageProcessor {
+
+  public void scaleImage(File source, File destination, int maxside)
+    throws IOException {
+    String size = "" + maxside + "x" + maxside;
+    String[] cmd = new String[] {"gm",
+                                 "convert",
+                                 "-size",
+                                 size,
+                                 "-resize",
+                                 size,
+                                 source.getPath(),
+                                 destination.getPath() };
+    Process convert = Runtime.getRuntime().exec(cmd, null, null);
+    try {
+      int result = convert.waitFor();
+      if (result != 0) {
+        InputStreamReader reader =
+          new InputStreamReader(convert.getErrorStream());
+        String msg = StreamUtils.read(reader);
+        throw new IOException("Error in conversion: " + msg);
+      }
+    } catch (InterruptedException e) {
+    }
+  }
+
+}
