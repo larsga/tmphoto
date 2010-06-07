d27069bbf4e1947c3f30eba66a9ae9495b616237
larsga <larsga@d0b0ecf4-2c1d-11de-ba90-e1c2dc4d389a>
2010-06-07 18:49:34 +0000

    Comment out the code to make it easier to compile. Doesn't work, anyway.

--- src/java/no/priv/garshol/topicmaps/tmphoto/images/JIGLProcessor.java
+++ src/java/no/priv/garshol/topicmaps/tmphoto/images/JIGLProcessor.java
@@ -4,13 +4,13 @@ package no.priv.garshol.topicmaps.tmphoto.images;
 import java.io.File;
 import java.io.IOException;
 
-import jigl.image.Image;
-import jigl.image.ImageNotSupportedException;
-import jigl.image.io.ImageInputStream;
-import jigl.image.io.ImageOutputStream;
-import jigl.image.io.ImageOutputStreamJAI;
-import jigl.image.io.IllegalPBMFormatException;
-import jigl.image.ops.levelOps.Scale;
+// import jigl.image.Image;
+// import jigl.image.ImageNotSupportedException;
+// import jigl.image.io.ImageInputStream;
+// import jigl.image.io.ImageOutputStream;
+// import jigl.image.io.ImageOutputStreamJAI;
+// import jigl.image.io.IllegalPBMFormatException;
+// import jigl.image.ops.levelOps.Scale;
   
 /**
  * Processor implemented using Java Image and Graphics Library (JIGL) 1.6.
@@ -19,23 +19,23 @@ public class JIGLProcessor implements ImageProcessor {
 
   public void scaleImage(File source, File destination, int maxside)
     throws IOException {
-    try {
-      ImageInputStream in = new ImageInputStream(source.getPath());
-      Image sourcei = in.read();
-      in.close();
+//     try {
+//       ImageInputStream in = new ImageInputStream(source.getPath());
+//       Image sourcei = in.read();
+//       in.close();
   
-      Image scaled = new Scale(maxside, maxside).apply(sourcei);
+//       Image scaled = new Scale(maxside, maxside).apply(sourcei);
       
-      ImageOutputStreamJAI out = new ImageOutputStreamJAI(destination.getPath());
-      out.writeJPEG(scaled);
-      // no close method. sigh.
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e); // not very likely, really
-    } catch (ImageNotSupportedException e) {
-      throw new RuntimeException(e); // not very likely, really
-    } catch (IllegalPBMFormatException e) {
-      throw new RuntimeException(e); 
-    }
+//       ImageOutputStreamJAI out = new ImageOutputStreamJAI(destination.getPath());
+//       out.writeJPEG(scaled);
+//       // no close method. sigh.
+//     } catch (InterruptedException e) {
+//       throw new RuntimeException(e); // not very likely, really
+//     } catch (ImageNotSupportedException e) {
+//       throw new RuntimeException(e); // not very likely, really
+//     } catch (IllegalPBMFormatException e) {
+//       throw new RuntimeException(e); 
+//     }
   }
 
 }
